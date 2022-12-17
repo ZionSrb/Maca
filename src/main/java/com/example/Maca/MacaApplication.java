729a853f3f08fac917c498d3c81e17885179ecf2
stodi <s.todic1810@gmail.com>
2022-12-17 20:02:43 +0100

    Initial commit

---
+++ src/main/java/com/example/Maca/MacaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Maca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Maca/MacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Maca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
